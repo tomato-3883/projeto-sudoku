1d09d64c5f33232db049192b1ea371f8b1b848cd
DaviR705 <rabelodavi38@gmail.com>
2025-07-22 20:40:50 -0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -145,61 +145,7 @@ def vitoria(matriz):
       
   return True
 
-#Trecho que recebe e lê o arquivo indicado pelo jogador, colocando as pistas no tabuleiro/grade
-if len(sys.argv) == 2:
-
- arq_entr = sys.argv[1]
- pistas = 0
-
- try:
-  with open(arq_entr, 'r')  as txt:
-
-    for linha in txt:
-      linha = linha.strip()
-      col, lin_num = linha.split(',')
-      lin, num = lin_num.split(':')
-
-      col_pst = posicao(col.strip()) - 1
-      lin_pst = int(lin.strip()) - 1
-      num = int(num.strip())
- 
-       #Garante a validade das pistas e acusa erro quando encontra uma pistas inválida
-      if 0 <= lin_pst < 9 and 0 <= col_pst < 9 and 1 <= num <=9:
-
-        #Acusa erro caso quebra as regras sobre linha, coluna e quadrante do Sudoku
-         if not jogada_valida(matriz, lin_pst, col_pst, num, nao_erropista = False):
-
-           matriz[lin_pst][col_pst] = (f'\033[1;31m{num :^3}\033[0m')
-           tabuleiro_sudoku(matriz)
-           print(f'\033[1;31mO arquivo {arq_entr} apresenta entradas inválidas.\033[0m')
-           sys.exit(1)
-
-        #Adiciona a pista caso ela esteja válida  
-         else:
-           matriz[lin_pst][col_pst] = num
-           pistas_tabuleiro[lin_pst][col_pst] = True
-           pistas +=1
-
-       #Acusa erro caso as pistas estejam fora do "range" do tabuleiro [A,i] nas colunas e [1,9] nas linhas e nos números
-      else:
-         print(f'\033[1;31mO arquivo {arq_entr} apresenta entradas inválidas (Fora dos intervalos [A,I] ou [1,9]).\033[0m')
-         sys.exit(1)
-
-   #Verifica se as pistas estam no intervalo de [1,80]
-  if pistas < 1 or pistas > 80:
-
-     print(f'\033[1;31mO número de pistas não está no intervalo [1,80].\033[0m')
-     sys.exit(1)
-
- #Acusa erro caso o arquivo informado não tenha sido encontrado
- except FileNotFoundError:
-  print(f'\033[1;31mO arquivo {arq_entr} não foi encontrado.\033[0m')
-  sys.exit(1)
-
-#MODO BATCH
-elif len(sys.argv) == 3:
-
- def traducao(entrada):
+def traducao(entrada):
     coord,valor = entrada.split(':')
     valor = int(valor)
     c,linha = coord.split(',')
@@ -232,7 +178,7 @@ elif len(sys.argv) == 3:
             return False
     return coluna,linha,valor
 
- def eh_valido(M,coluna,linha,valor):
+def eh_valido(M,coluna,linha,valor):
     for j in range(9): #Verificando se há valores iguais na linha
         if M[linha][j] == valor:
             return False
@@ -248,14 +194,14 @@ elif len(sys.argv) == 3:
                 return False
     return True
 
- def resolvido(M): #Para sabermos se um determinado jogo está completo, precisamos verificar se um elemento da matriz ainda é 0
+def resolvido(M): #Para sabermos se um determinado jogo está completo, precisamos verificar se um elemento da matriz ainda é 0
     for i in range(9):
         for j in range(9):
             if M[i][j] == 0:
                 return False
     return True
 
- def preencher_de_dicas(arquivo,M):
+def preencher_de_dicas(arquivo,M):
     with open(arquivo,'r') as lista_dicas: #Abrir o arquivo de dicas
         for dica in lista_dicas: #Pega cada dica da lista inteira de dicas
             coluna,linha,valor = traducao(dica) #Traduz a dica para algo usavel
@@ -265,7 +211,7 @@ elif len(sys.argv) == 3:
                 return False #Se a dica for inválida, vai ser o gatilho para o arquivo de dicas ser inválido
     return M
 
- def preencher_de_respostas(arquivo,M):
+def preencher_de_respostas(arquivo,M):
     with open(arquivo,'r') as lista_respostas:
         invalido = []
         for resposta in lista_respostas:
@@ -276,6 +222,8 @@ elif len(sys.argv) == 3:
             else:
                 invalido.append(f'A jogada ({coluna_str},{linha+1}) = {valor} é inválida!')
     return invalido, resolvido(M)
+#MODO BATCH
+if len(sys.argv) == 3:
 
  M = [[0]*9 for _ in range(9)] #Matriz que será usada para armazenar os dados
 
@@ -299,116 +247,320 @@ elif len(sys.argv) == 3:
         print('A grade não foi preenchida!')
         sys.exit(0)
 
-  
-#Printa o tabuleiro a primeira vez já com as pista colocadas
-tabuleiro_sudoku(matriz)
-
-#Loop para computar e verificar as jogadas feitas pelo usuário/jogador
-while True:
+#Trecho que recebe e lê o arquivo indicado pelo jogador, colocando as pistas no tabuleiro/grade
+elif len(sys.argv) == 2:
  
- jogada = input('Faça sua jogada (coluna,linha:número): ')
+ modo = input('\033[1;34mQual modo você deseja iniciar, interativo ou solucionador? (i,s) \033[0m')
+ print()
  
- #Verifica que o jogador usou o comando para pedir as possibilidades de jogadas para uma casa
- if jogada.startswith('?'):
-   jogada = jogada[1:] 
+ if modo == 's':
 
-   co, li = jogada.split(',')
-   co = posicao(co) - 1
-   li = int(li) - 1
+   #MODO SOLUCIONADOR
+   def montar_grade_do_arquivo(caminho_arquivo): #lê o arquivo de pistas e monta a grade do arquivo
+    grade = [[0 for _ in range(9)] for _ in range(9)]
+    pistas_marcadas = [[False for _ in range(9)] for _ in range(9)]
+    
+    try:
+        with open(caminho_arquivo, 'r') as f:
+            for linha_str in f:
+                linha_limpa = linha_str.strip()
+                if linha_limpa and not linha_limpa.startswith('#'):
+                    try:
+                        partes = linha_limpa.split(':')
+                        coordenada = partes[0].split(',')
+                        valor = int(partes[1].strip())
+                        
+                        letra_col = coordenada[0].strip().upper()
+                        num_lin = int(coordenada[1].strip())
 
-   #Impede o comando e aponta erro caso seja uma casa de pista ou já esteja ocupada
-   if 0 <= li < 9 and 0 <= co < 9:
-     if matriz[li][co] != 0 or pistas_tabuleiro[li][co]:
-        print()
-        print('\033[1;31mEssa casa já está ocupada! Esse comando só funciona para casas vazias.\033[0m')
-     
-     #Analisa e dá as jogadas possíveis para a casa solicitada
-     else:
-        print()
-        print(f"\n\033[1;34mJogadas possíveis para ({jogada}):\033[0m ", end= '')
+                        col = ord(letra_col) - ord('A')
+                        lin = num_lin - 1
+                        
+                        #acusa erro caso leia pistas inválidas dos arquivos
+                        if grade[lin][col] !=0 or not verificar_movimento_valido(grade, valor, lin, col):
+                            print('\033[1;31mO arquivo de pistas apresenta pistas inválidas.\033[0m')
+                            sys.exit(1)
+                        
+                        grade[lin][col] = valor
+                        pistas_marcadas[lin][col] = True
+
+                    except (ValueError, IndexError):
+                        print(f"Ignorando linha com formato inválido '{linha_limpa}'")
+
+    except FileNotFoundError:
+        print(f"Arquivo não encontrado: {caminho_arquivo}")
+        return None, None
+    
+    return grade, pistas_marcadas
+
+   def imprimir_tabuleiro(matriz, pistas_marcadas):
+    print('     A   B   C    D   E   F    G   H   I')
+    print('  ++---+---+---++---+---+---++---+---+---++')
+    for li in range(9):
+        if li % 3 == 0 and li != 0:
+            print('  ++===+===+===++===+===+===++===+===+===++')
+        elif li != 0:
+            print('  ++---+---+---++---+---+---++---+---+---++')
         
-        for n in range(1, 10):
+        print(f'{li + 1} ||', end='')
+
+        for co in range(9):
+            if co != 0:
+                if co % 3 == 0:
+                    print('||', end='')
+                else:
+                    print('|', end='')
+
+            num = matriz[li][co]
+            if num != 0:
+                if pistas_marcadas[li][co]:
+                    print(f"\033[1;31m{num:^3}\033[0m", end='')
+                else:
+                    print(f"{num:^3}", end='')
+            else:
+                print(f"{'':^3}", end='')
+
+        print(f'|| {li + 1}')
+    print('  ++---+---+---++---+---+---++---+---+---++')
+    print('     A   B   C    D   E   F    G   H   I')
+
+
+   def verificar_movimento_valido(matriz, numero, linha, coluna):
+    for i in range(9):
+        if matriz[linha][i] == numero or matriz[i][coluna] == numero:
+            return False
+    quadrante_linha = (linha // 3) * 3
+    quadrante_coluna = (coluna // 3) * 3
+    for l in range(3):
+        for c in range(3):
+            if matriz[quadrante_linha + l][quadrante_coluna + c] == numero:
+                return False
+    return True
+
+   def opcoes_possiveis(matriz, linha, coluna): #vê quais números podem ser usados e guarda numa lista
+    if matriz[linha][coluna] != 0:
+        return []
+
+    opcoes = []
+    for numero in range(1, 10):
+        if verificar_movimento_valido(matriz, numero, linha, coluna):
+            opcoes.append(numero)
+    return opcoes
+
+   #função que tenta resolver a matriz sem tentativa e erro, só com as dicas
+   def resolver_sudoku(matriz, pistas_marcadas):
+    matriz_ant = [[-1 for _ in range(9)] for _ in range(9)]
+
+    while matriz != matriz_ant: #faz uma cópia da matriz atual para comparar depois
+
+        #cria uma cópia de cada linha da matriz real, pra não deixar que as mudanças em uma matriz alterem a outra
+        matriz_ant = [linha[:] for linha in matriz] 
+
+        for linha in range(9):
+            for coluna in range(9):
+                if matriz[linha][coluna] == 0:
+                    opcoes = opcoes_possiveis(matriz, linha, coluna)
+                    if len(opcoes) == 1:
+                        matriz[linha][coluna] = opcoes[0]
+                        pistas_marcadas[linha][coluna] = False
+
+    #vê se todo o tabuleiro foi completado
+    for linha in range(9):
+        for coluna in range(9):
+            if matriz[linha][coluna] == 0:
+                return False
+    return True
+
+   caminho_do_arquivo = sys.argv[1]
+        
+   matriz_jogo, pistas_marcadas = montar_grade_do_arquivo(caminho_do_arquivo) #carrega a matriz do jogo e a marcação das pistas
+
+   if matriz_jogo:
+            print("--- Módulo Solucionador ---")
+            print(f"\nTabuleiro inicial do arquivo {caminho_do_arquivo}")
+            imprimir_tabuleiro(matriz_jogo, pistas_marcadas)
+
+            confirmacao = input("\nPressione ENTER para resolver ou 'N' para cancelar: ").upper() #essa parte é a responsável por perguntar se a resposta do sudoku deve ser mostrada ou não
+            
+            if confirmacao != 'N':
+                print("\nIniciando a solução")
+                sucesso = resolver_sudoku(matriz_jogo, pistas_marcadas)
+                print('Grade preenchida com base nas pistas:')
+                imprimir_tabuleiro(matriz_jogo, pistas_marcadas)
+
+                if not sucesso:
+                    print()
+                    print('\033[1;31mNão foi possível continuar, quantidade de pistas insuficiente.\033[0m')
+
+                    sys.exit(1)
+                else:
+                    print()
+                    print('\033[1;32mSudoku solucionado com sucesso!\033[0m')
+                    sys.exit(0)
+            else:
+                print("Operação cancelada.")
+                sys.exit(0)
+
+ elif modo == 'i':
+
+  #MODO INTERATIVO
+  arq_entr = sys.argv[1]
+  pistas = 0
+
+  try:
+   with open(arq_entr, 'r')  as txt:
+
+     for linha in txt:
+      linha = linha.strip()
+      col, lin_num = linha.split(',')
+      lin, num = lin_num.split(':')
+
+      col_pst = posicao(col.strip()) - 1
+      lin_pst = int(lin.strip()) - 1
+      num = int(num.strip())
+ 
+       #Garante a validade das pistas e acusa erro quando encontra uma pistas inválida
+      if 0 <= lin_pst < 9 and 0 <= col_pst < 9 and 1 <= num <=9:
+
+        #Acusa erro caso quebra as regras sobre linha, coluna e quadrante do Sudoku
+         if not jogada_valida(matriz, lin_pst, col_pst, num, nao_erropista = False):
+
+           matriz[lin_pst][col_pst] = (f'\033[1;31m{num :^3}\033[0m')
+           tabuleiro_sudoku(matriz)
+           print(f'\033[1;31mO arquivo {arq_entr} apresenta entradas inválidas.\033[0m')
+           sys.exit(1)
+
+        #Adiciona a pista caso ela esteja válida  
+         else:
+           matriz[lin_pst][col_pst] = num
+           pistas_tabuleiro[lin_pst][col_pst] = True
+           pistas +=1
+
+       #Acusa erro caso as pistas estejam fora do "range" do tabuleiro [A,i] nas colunas e [1,9] nas linhas e nos números
+      else:
+         print(f'\033[1;31mO arquivo {arq_entr} apresenta entradas inválidas (Fora dos intervalos [A,I] ou [1,9]).\033[0m')
+         sys.exit(1)
+
+   #Verifica se as pistas estam no intervalo de [1,80]
+   if pistas < 1 or pistas > 80:
+
+     print(f'\033[1;31mO número de pistas não está no intervalo [1,80].\033[0m')
+     sys.exit(1)
+
+  #Acusa erro caso o arquivo informado não tenha sido encontrado
+  except FileNotFoundError:
+   print(f'\033[1;31mO arquivo {arq_entr} não foi encontrado.\033[0m')
+   sys.exit(1)
+
+  #Printa o tabuleiro a primeira vez já com as pista colocadas
+  tabuleiro_sudoku(matriz)
+
+  #Loop para computar e verificar as jogadas feitas pelo usuário/jogador
+  while True:
+ 
+   jogada = input('Faça sua jogada (coluna,linha:número): ')
+ 
+   #Verifica que o jogador usou o comando para pedir as possibilidades de jogadas para uma casa
+   if jogada.startswith('?'):
+     jogada = jogada[1:] 
+
+     co, li = jogada.split(',')
+     co = posicao(co) - 1
+     li = int(li) - 1
+
+     #Impede o comando e aponta erro caso seja uma casa de pista ou já esteja ocupada
+     if 0 <= li < 9 and 0 <= co < 9:
+       if matriz[li][co] != 0 or pistas_tabuleiro[li][co]:
+         print()
+         print('\033[1;31mEssa casa já está ocupada! Esse comando só funciona para casas vazias.\033[0m')
+     
+       #Analisa e dá as jogadas possíveis para a casa solicitada
+       else:
+         print()
+         print(f"\n\033[1;34mJogadas possíveis para ({jogada}):\033[0m ", end= '')
+        
+         for n in range(1, 10):
             if jogada_valida(matriz, li, co, n, nao_erropista = False):
                 print(f'\033[1;34m{n} \033[0m', end= '')
-        print('\n')
+         print('\n')
 
-   #Apresenta o erro caso o comando seja usado para uma casa que não existe
-   else:
-      print()
-      print('\033[1;31mPosição fora do range do tabuleiro [A,I][1,9]. Faça uma nova jogada.'
-         + '\033[0m')
+       #Apresenta o erro caso o comando seja usado para uma casa que não existe
+     else:
+       print()
+       print('\033[1;31mPosição fora do range do tabuleiro [A,I][1,9]. Faça uma nova jogada.'
+          + '\033[0m')
 
- #Verifica que o jogador usou o comando para apagar uma das jogadas anteriores
- elif jogada.startswith('!'):
-   jogada = jogada[1:]
+   #Verifica que o jogador usou o comando para apagar uma das jogadas anteriores
+   elif jogada.startswith('!'):
+      jogada = jogada[1:]
 
-   co, li = jogada.split(',')
-   co = posicao(co) - 1
-   li = int(li) - 1
+      co, li = jogada.split(',')
+      co = posicao(co) - 1
+      li = int(li) - 1
 
-   if 0 <= li < 9 and 0 <= co <9:
+      if 0 <= li < 9 and 0 <= co <9:
 
-    #Verfica se a casa é uma pista e impede o comando
-    if pistas_tabuleiro[li][co]:
-      print()
-      print('\033[1;31mEssa casa é uma pista e não pode ser alterada! Faça uma nova jogada.\033[0m')
+       #Verfica se a casa é uma pista e impede o comando
+       if pistas_tabuleiro[li][co]:
+        print()
+        print('\033[1;31mEssa casa é uma pista e não pode ser alterada! Faça uma nova jogada.\033[0m')
    
-    #Verifica se a casa está vazia e impede o comando
-    elif matriz[li][co] == 0:
-      print()
-      print('\033[1;31mEssa casa já está vazia!\033[0m')
+       #Verifica se a casa está vazia e impede o comando
+       elif matriz[li][co] == 0:
+        print()
+        print('\033[1;31mEssa casa já está vazia!\033[0m')
 
-    #Apaga a jogada caso não tenha nenhum dos problemas anteriores
-    else:
-      matriz[li][co] = 0
-      print()
-      print('\033[1;34mJogada apagada.\033[0m')
+       #Apaga a jogada caso não tenha nenhum dos problemas anteriores
+       else:
+        matriz[li][co] = 0
+        print()
+        print('\033[1;34mJogada apagada.\033[0m')
 
-   #Apresenta o erro caso o comando seja usado para uma casa que não existe
+      #Apresenta o erro caso o comando seja usado para uma casa que não existe
+      else:
+        print()
+        print('\033[1;31mPosição fora do range do tabuleiro [A,I][1,9]. Faça uma nova jogada.'
+          + '\033[0m')
+      
+   #Caso a entrada não seja nem um comando específico, faz a verificação da jogada de fato
    else:
+
+    co, li_num = jogada.split(',')
+    li, num = li_num.split(':')
+    co = posicao(co)
+
+    co = int(co) - 1
+    li = int(li) - 1
+    num = int(num)
+
+    #Praticamente as mesmas verificações e mensagens de erros dos outros comandos
+    if 0 <= li < 9 and 0 <= co < 9 and 1 <= num <=9:
+
+      if pistas_tabuleiro[li][co]:
+       print()
+       print('\033[1;31mEssa casa é uma pista e não pode ser alterada! Faça uma nova jogada.\033[0m')
+
+      elif matriz[li][co] != 0:
+       print()
+       sobrescrever = input(f'\033[1;34mEssa casa já está ocupada. Deseja sobrescrever o número? (s/n) \033[0m')
+
+       if sobrescrever == 's':
+        if jogada_valida(matriz, li, co, num):
+         matriz[li][co] = num
+        
+      elif jogada_valida(matriz, li, co, num):
+        matriz[li][co] = num
+
+    else:
       print()
       print('\033[1;31mPosição fora do range do tabuleiro [A,I][1,9]. Faça uma nova jogada.'
-         + '\033[0m')
-      
- #Caso a entrada não seja nem um comando específico, faz a verificação da jogada de fato
- else:
+            + '\033[0m')
 
-  co, li_num = jogada.split(',')
-  li, num = li_num.split(':')
-  co = posicao(co)
-
-  co = int(co) - 1
-  li = int(li) - 1
-  num = int(num)
-
-  #Praticamente as mesmas verificações e mensagens de erros dos outros comandos
-  if 0 <= li < 9 and 0 <= co < 9 and 1 <= num <=9:
-
-   if pistas_tabuleiro[li][co]:
-    print()
-    print('\033[1;31mEssa casa é uma pista e não pode ser alterada! Faça uma nova jogada.\033[0m')
-
-   elif matriz[li][co] != 0:
-     print()
-     sobrescrever = input(f'\033[1;34mEssa casa já está ocupada. Deseja sobrescrever o número? (s/n) \033[0m')
-
-     if sobrescrever == 's':
-       if jogada_valida(matriz, li, co, num):
-        matriz[li][co] = num
-        
-   elif jogada_valida(matriz, li, co, num):
-    matriz[li][co] = num
-
-  else:
    print()
-   print('\033[1;31mPosição fora do range do tabuleiro [A,I][1,9]. Faça uma nova jogada.'
-         + '\033[0m')
+   tabuleiro_sudoku(matriz)
 
- print()
- tabuleiro_sudoku(matriz)
-
- #Verifica a vitória e dá os parabéns ao jogador
- if vitoria(matriz):
+   #Verifica a vitória e dá os parabéns ao jogador
+   if vitoria(matriz):
       print('\n\033[1;32mSudoku concluido! Parabéns!\033[0m\n')
       sys.exit(0)
 
